8ab0076e651a98f629bdf4dc9a5cee0a5e902c6e
MarlonMoreno <marlonmoreno894@gmail.com>
2021-05-18 21:52:15 -0500

    commit tree 3a7dae9b39feca1142d5a07a5a6e35b6352690b4

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(d =>
+        d.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter( d =>
+        d.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+function matchName(array, string) {
+    return array.filter( d =>
+        d.name === string
+    )
+}
